9a558c9794ac5946b7038d1f6140b517a028d55d
Viktor Szépe <viktor@szepe.net>
2020-10-06 11:46:20 +0000

    Analyte the trait too!

---
+++ tests/AnalyzeTrait.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * @package Fluidity
+ * @license http://opensource.org/licenses/MIT
+ */
+
+declare(strict_types=1);
+
+namespace DecodeLabs\Fluidity\Tests;
+
+use DecodeLabs\Fluidity\Then;
+use DecodeLabs\Fluidity\ThenTrait;
+
+class AnalyzeTrait implements Then
+{
+    use ThenTrait;
+
+    public function then(callable $callback): Then
+    {
+        return $this;
+    }
+
+    public function thenEach(array $values, callable $callback): Then
+    {
+        return $this;
+    }
+
+    public function thenIf($truth, callable $yes, callable $no = null): Then
+    {
+        return $this;
+    }
+
+    public function thenUnless($truth, callable $no, callable $yes = null): Then
+    {
+        return $this;
+    }
+};
