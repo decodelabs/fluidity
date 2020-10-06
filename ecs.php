99fd55fc2797d34b2aac7ed71c72891d616cb0e3
Tom Wright <tom@inflatablecookie.com>
2020-10-06 13:22:47 +0000

    Applied full PSR12 standards
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ ecs.php
@@ -0,0 +1,15 @@
+<?php
+
+// ecs.php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::SETS, [SetList::CLEAN_CODE, SetList::PSR_12]);
+};
