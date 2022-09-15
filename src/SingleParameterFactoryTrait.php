0763d7fc9e2699c7918627811448f5f5c6cab4d4
Tom Wright <tom@inflatablecookie.com>
2022-09-15 10:11:11 +0000

    Added SingleParameterFactory interface
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ src/SingleParameterFactoryTrait.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @package Fluidity
+ * @license http://opensource.org/licenses/MIT
+ */
+
+declare(strict_types=1);
+
+namespace DecodeLabs\Fluidity;
+
+use Throwable;
+
+/**
+ * @template TInput
+ */
+trait SingleParameterFactoryTrait
+{
+    /**
+     * @phpstan-param TInput $value
+     */
+    public static function instance(mixed $value): static
+    {
+        return new static($value);
+    }
+
+    /**
+     * @phpstan-param TInput|null $value
+     */
+    public static function orNull(mixed $value): ?static
+    {
+        if ($value === null) {
+            return null;
+        }
+
+        try {
+            return static::instance($value);
+        } catch (Throwable $e) {
+            return null;
+        }
+    }
+}
